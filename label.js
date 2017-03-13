057e0ae8239aa5afed109f92bad841c998989b8d
udanname <udan.habirau@gmail.com>
2017-03-13 18:30:22 +0700

    Add files via upload

---
+++ label.js
@@ -0,0 +1,30 @@
+
+var numposts = 300;
+var standardstyling = true;
+
+function startpost(json){
+  for (var i = 0; i < numposts; i++){
+    var entry = json.feed.entry[i];
+    var posttitle = entry.title.$t;
+    var posturl;
+    
+    if (i == json.feed.entry.length) break;
+    
+    for (var k = 0; k < entry.link.length; k++){
+      if (entry.link[k].rel == 'alternate'){
+        posturl = entry.link[k].href;break;
+      }
+    }
+        
+    posttitle = posttitle.link(posturl);
+    if (standardstyling) document.write('<li>');
+    
+    document.write(posttitle);
+  }
+  
+  if (standardstyling) document.write('</li>');
+}
+
+function finished(){
+  document.write('<br /><br /><a href="http://www.udan.name" style="font-size: 10 px; text-decoration:none; color: #616469;" target="_blank" title="udan">Sitemap</a>');
+}
